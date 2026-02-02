a6175835d2e54a9eca0a8de5270bfd83ab927a36
jooky <jooky0327@naver.com>
2026-02-02 17:20:37 +0900

    feat: user 도메인 설정

---
+++ src/main/java/com/back/domain/user/user/entity/Gender.java
@@ -0,0 +1,7 @@
+package com.back.domain.user.user.entity;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    UNKNOWN
+}
--- src/main/java/com/back/domain/user/user/entity/User.java
+++ src/main/java/com/back/domain/user/user/entity/User.java
@@ -17,11 +17,33 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Gender gender = Gender.UNKNOWN;
+
+    @Column(nullable = false)
+    private double height;
+
+    @Column(nullable = false)
+    private double weight;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Role role = Role.USER;
+
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
